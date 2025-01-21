5f65d6d1ac76a19483e4361474e05c6c365af228
kitijakronberga <kronbergakitija@gmail.com>
2025-01-21 09:46:21 +0000

    v01

---
+++ otrais.py
@@ -0,0 +1,8 @@
+skaitlis=int(input("Ievadiet skaitli: "))
+print(type(skaitlis))
+if skaitlis>0:
+    print("Skaitlis poz")
+elif skaitlis==0:
+    print("Skaitlis ir 0")
+else:
+    print("Skaitlis neg")        
